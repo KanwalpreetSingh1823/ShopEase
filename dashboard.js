6a841e258370b753ce0dfed0a64c810d132c846d
KanwalpreetSingh1823 <kanwalpreets17@gmail.com>
2024-07-19 23:19:14 +0530

    Adding Item-Count and Modifying Cloth Section

--- dashboard.js
+++ dashboard.js
@@ -24,28 +24,23 @@ document.addEventListener("DOMContentLoaded",()=>{
 
     let i = 0;
     let j = 0;
-    function autoType1(){
+    function autoType(){
         if(i<text.length){
             head.innerHTML += text.charAt(i);
             i++;
-            setTimeout(autoType1,100);
         }
-    }
-    function autoType2(){
         if(j<text2.length){
             head2.innerHTML += text2.charAt(j);
             j++;
-            setTimeout(autoType2,100);
+        }
+        if(i<text.length || j<text2.length){
+            setTimeout(autoType,100);
         }
     }
-    autoType1();
-    autoType2();  //Initial Call to start the typing test
+    autoType(); //Initial Call to start the typing test
 })
 
-const product = document.querySelector(".item");
 const clothSec = document.querySelector(".cloth-section");
-const itemPic = document.querySelector(".product");
-// const desc = document.querySelector(".des");  //description
 
 const images = [
     'pic1.jpg', 'pic2.jpg',
@@ -53,24 +48,72 @@ const images = [
     'pic5.jpg', 'pic6.jpg',
     'pic7.jpg', 'pic8.jpg'
 ]
-const description = [
-    'pic1.jpg', 'pic2.jpg',
-    'pic3.jpg', 'pic4.jpg',
-    'pic5.jpg', 'pic6.jpg',
-    'pic7.jpg', 'pic8.jpg'
+
+const proPrice = [
+    'price = ₹599', 'price = ₹599',
+    'price = ₹599', 'price = ₹599',
+    'price = ₹599', 'price = ₹599',
+    'price = ₹599', 'price = ₹599'
 ]
-for(let i=1; i<=images.length; i++){
+const proDescription = [
+    'This is the description of product', 'This is the description of product',
+    'This is the description of product', 'This is the description of product',
+    'This is the description of product', 'This is the description of product',
+    'This is the description of product', 'This is the description of product'
+]
+
+for(let i=0; i<images.length; i++){
     const flexItem = document.createElement('div');
-    flexItem.classList.add('product');
+    flexItem.classList.add('item');
 
     const image = document.createElement('img');
     image.src = images[i];
-    image.classList.add('itemPic');
+    image.classList.add('product');
     flexItem.appendChild(image);
 
     const descrip = document.createElement('p');
-    descrip.innerHTML = description[i];
-    // descrip.classList.add('itemPic');
+    const price = document.createElement('p');
+    descrip.innerHTML = proDescription[i];
+    price.innerHTML = proPrice[i];
+    descrip.classList.add('disc');
+    price.classList.add('price');
+
+    const descr2 = document.createElement('div');
+    const selectItem = document.createElement('i');
+
+    selectItem.classList.add('fa-regular', 'fa-heart');
+    selectItem.classList.add('select-item');
+
+    descr2.appendChild(price);
+    descr2.appendChild(selectItem);
+    descr2.classList.add('desc2');
+
     flexItem.appendChild(descrip);
+    flexItem.appendChild(descr2);
+
     clothSec.append(flexItem);
+
+
 }
+
+let count = 0;   //Item-count
+const selected_item = document.querySelectorAll(".select-item");
+let itemCount = document.querySelector(".item-count");
+itemCount.innerHTML = count;
+selected_item.forEach((val) => {
+   val.addEventListener("click",()=>{
+    if(val.classList.contains('fa-regular')){
+        val.classList.add('fa-solid');
+        val.classList.remove('fa-regular');
+        val.style.color = "red";
+        count++;
+       }
+    else if(val.classList.contains('fa-solid')){
+        val.classList.remove('fa-solid');
+        val.classList.add('fa-regular');
+        val.style.color = "";
+        count--;
+       }
+       itemCount.innerHTML = count;
+   })
+});
